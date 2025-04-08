b513bd67351bee1918d48029b6d79c33bd1a4037
Henrik Petersen <hpet@deop0509.autoconnect.de>
2025-04-08 14:18:04 +0200

    added blob function js

--- scripts/date_input.js
+++ scripts/date_input.js
@@ -1,4 +1,16 @@
 
+const fileInput = document.getElementById('file-input');
+fileInput.addEventListener('change', () => {
+    const files = fileInput.files;
+    if(files.length > 0) {
+        Array.from(files).forEach(async file => {
+            const blob = new Blob([file], {type: file.type});
+            const text = await blob.text();
+            console.log('Neue Datei', text)
+        })
+    }
+})
+
 function openUpload() {
     document.getElementById('file-input').click();
 }
